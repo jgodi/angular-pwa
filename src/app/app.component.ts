303ed3b5164517863afb25858d127a507279f25f
Joshua Godi <joshuagodi@gmail.com>
2018-01-10 12:08:46 -0500

    Auto-generated commit

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'meow-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-}
